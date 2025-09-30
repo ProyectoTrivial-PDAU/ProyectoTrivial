a70b9198cde033c16cc07a35b925006d303a84f2
Carlos Lopez <162631408+KratosOps@users.noreply.github.com>
2025-09-30 18:05:20 +0200

    Add files via upload

---
+++ ProyectoTrivial/src/test/java/com/example/ProyectoTrivial/ProyectoTrivialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoTrivial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTrivialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
